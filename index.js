a9962c4088c6c1a1258bbd7f334cd9cc341fbd58
anna <anna@rcbd.org>
2023-05-03 09:48:19 -0400

    Use for loop

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+// ✅ Variant 1
+// console.log("Timer...");
+// alert("One, two, three... GO!");
+// alert(5);
+// alert(4);
+// alert(3);
+// alert(2);
+// alert(1);
+// alert(0);
+// alert("Reverse counting finished");
+
+// console.log("Reverse counting finished");
+
+// ✅ Variant 2
 console.log("Timer...");
 alert("One, two, three... GO!");
-alert(5);
-alert(4);
-alert(3);
-alert(2);
-alert(1);
-alert(0);
-alert("Reverse counting finished");
 
+for (let i = 5; i >= 0; i--) {
+  alert(i);
+}
+
+alert("Reverse counting finished");
 console.log("Reverse counting finished");
