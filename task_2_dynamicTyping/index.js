7835617abfcb3f4ce3953f26fd69991e3a62b654
anna <anna@rcbd.org>
2023-05-03 13:09:48 -0400

    Add task_2_dynamicTyping

---
+++ task_2_dynamicTyping/index.js
@@ -0,0 +1,12 @@
+// number
+let number = 15;
+// string
+let string = "JS";
+// boolean
+let boolean = true;
+
+number = "Not number anymore";
+string = false;
+boolean = 15;
+
+console.log(number, string, boolean);
