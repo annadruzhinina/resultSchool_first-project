f4a6793277e010b702352f34c5aa5813a730afa5
anna <anna@rcbd.org>
2023-05-03 10:40:25 -0400

    Resolve Prompt task

---
+++ prompt/index.js
@@ -0,0 +1,20 @@
+const question1 = prompt("Сколько будет 2 + 2?");
+if (question1 === "4") {
+  alert("Your answer is correct!");
+} else {
+  alert("Sorry but your answer is wrong 😕");
+}
+
+const question2 = prompt("Чему равен корень из 9?");
+if (question2 === "3") {
+  alert("Your answer is correct!");
+} else {
+  alert("Sorry but your answer is wrong 😕");
+}
+
+const question3 = prompt("Сколько будет 2 в 5-й степени?");
+if (question3 === "32") {
+  alert("Your answer is correct!");
+} else {
+  alert("Sorry but your answer is wrong 😕");
+}
---
+++ task_3/js-info.js
@@ -0,0 +1,15 @@
+alert(
+  "Who developed the JavaScript programming language?\n ✅ Answer: JavaScript was invented by Brendan Eich in 1995. It was developed for Netscape 2, and became the ECMA-262 standard in 1997."
+);
+alert(
+  "In how many days was JavaScript developed?\n ✅ Answer:  Designing a Language in 10 Days. The evolution and use of JavaScript, a language developed in 10 days back in 1995, is really just getting started"
+);
+alert(
+  "What year was JavaScript introduced?\n ✅ Answer: JavaScript was invented by Brendan Eich in 1995. It was developed for Netscape 2, and became the ECMA-262 standard in 1997."
+);
+alert(
+  `Is JavaScript the same as Java? (answer either "JavaScript is Java" or "JavaScript is not Java"\n ✅ Answer: No!`
+);
+alert(
+  `What is the name of the JavaScript specification?\n ✅ Answer: The core language of JavaScript is standardized by the ECMA TC39 committee as a language named ECMAScript. "ECMAScript" is the term for the language standard, but "ECMAScript" and "JavaScript" can be used interchangeably.`
+);
