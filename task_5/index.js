440733f7895f2d8595006c4d166341346be0fc87
anna <anna@rcbd.org>
2023-05-03 11:29:39 -0400

    Task_5

---
+++ task_5/index.js
@@ -0,0 +1,13 @@
+// JavaScript-code to create variables
+
+const myName = "Anna";
+const mySurname = "Druzhinina";
+let myFavoriteDrink = "Tea";
+let myFavoriteAnimal = "Cat"
+let myFavoriteProgrammingLanguage = "JavaScript";
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
