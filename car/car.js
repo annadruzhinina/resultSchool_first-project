6cdcaae959ff68003d0478f627d018fa2dc5be35
anna <anna@rcbd.org>
2023-05-03 11:56:25 -0400

    Car

--- car/car.js
+++ car/car.js
@@ -4,4 +4,14 @@ let carOwner = "Anna";
 
 alert("Car model: " + carName);
 alert("Max Car Speed: " + maxCarSpeed);
-alert("Car Owner: " + mcarOwner);
+alert("Car Owner: " + carOwner);
+
+carOwner = "Masha";
+
+maxCarSpeed = "400km/h";
+alert("Updated Max Car Speed: " + maxCarSpeed);
+console.log(`The car's speed has been changed`, maxCarSpeed);
+
+maxCarSpeed = "450km/h";
+alert("Updated Max Car Speed: " + maxCarSpeed);
+console.log(`The car's speed has been changed`, maxCarSpeed);
