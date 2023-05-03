4090fc93fce67deb42723884b95a5a2e1999e83e
anna <anna@rcbd.org>
2023-05-03 09:30:10 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Timer...");
+alert("One, two, three... GO!");
+alert(5);
+alert(4);
+alert(3);
+alert(2);
+alert(1);
+alert(0);
+alert("Reverse counting finished");
+
+console.log("Reverse counting finished");
