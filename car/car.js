e61fdf6a5707339d545502d7e2cbdf703ec4fc44
anna <anna@rcbd.org>
2023-05-03 11:38:32 -0400

    Car

---
+++ car/car.js
@@ -0,0 +1,7 @@
+const carName = "Volvo";
+let maxCarSpeed = "350km/h";
+let carOwner = "Anna";
+
+alert("Car model: " + carName);
+alert("Max Car Speed: " + maxCarSpeed);
+alert("Car Owner: " + mcarOwner);
