b9685c1ee2b8ad55b989341a2fad394cb749c851
anna <anna@rcbd.org>
2023-05-05 10:53:54 -0400

    Add javaScriptTypeConversions file

--- javaScriptTypeConversions /index.js
+++ javaScriptTypeConversions /index.js
@@ -37,16 +37,59 @@ console.log(Number(distanceMoon), Boolean(distanceMoon), String(distanceMoon));
 // result: 5 true 5
 
 // Symbol
-let obj = { key: "anna" }; // объект
-console.log(Number(obj), Boolean(obj), String(obj));
+let obj_ = { key: "anna" }; // объект
+console.log(Number(obj_), Boolean(obj_), String(obj_));
 // result: NaN true [object Object]
-let arr = [1, 2, 3]; // массив
-console.log(Number(arr), Boolean(arr), String(arr));
+let arr_ = [1, 2, 3]; // массив
+console.log(Number(arr_), Boolean(arr_), String(arr_));
 // result: NaN true 1,2,3
 
 // Function
-let func = function () {
+let func_ = function () {
   return 1 + 1;
 };
-console.log(Number(func), Boolean(func), String(func));
+console.log(Number(func_), Boolean(func_), String(func_));
 // result: NaN true function () {return 1 + 1;}
+
+// OR
+// объявление и инициализация переменных разных типов данных
+let num = 42; // число
+let str = "Hello, world!"; // строка
+let bool = true; // булево значение
+let nul = null; // значение null
+let undef = undefined; // неопределенное значение
+let obj = { key: "value" }; // объект
+let arr = [1, 2, 3]; // массив
+let func = function () {
+  return "function";
+}; // функция
+
+// явное преобразование к числовому типу
+console.log(Number(num)); // 42
+console.log(Number(str)); // NaN
+console.log(Number(bool)); // 1
+console.log(Number(nul)); // 0
+console.log(Number(undef)); // NaN
+console.log(Number(obj)); // NaN
+console.log(Number(arr)); // NaN
+console.log(Number(func)); // NaN
+
+// явное преобразование к булевому типу
+console.log(Boolean(num)); // true
+console.log(Boolean(str)); // true
+console.log(Boolean(bool)); // true
+console.log(Boolean(nul)); // false
+console.log(Boolean(undef)); // false
+console.log(Boolean(obj)); // true
+console.log(Boolean(arr)); // true
+console.log(Boolean(func)); // true
+
+// явное преобразование к строковому типу
+console.log(String(num)); // "42"
+console.log(String(str)); // "Hello, world!"
+console.log(String(bool)); // "true"
+console.log(String(nul)); // "null"
+console.log(String(undef)); // "undefined"
+console.log(String(obj)); // "[object Object]"
+console.log(String(arr)); // "1,2,3"
+console.log(String(func)); // "function() { return "function"; }"
