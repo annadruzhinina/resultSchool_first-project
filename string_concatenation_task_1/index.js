95dbb14ae0c5de171b7852b0b6d09463ab689753
anna <anna@rcbd.org>
2023-05-05 21:29:20 -0400

    Add task about concatination

---
+++ string_concatenation_task_1/index.js
@@ -0,0 +1,17 @@
+const myName = "Anna";
+const programmingLanguage = "Java Script";
+const courseCreatorName = "Course Creater Name";
+const reasonText = "Get new knowledge";
+const numberOfMonth = "8 months";
+
+console.log(
+  `${myName} ${programmingLanguage} ${courseCreatorName} ${reasonText} ${numberOfMonth}`
+);
+
+console.log(
+  myName + ' ' +
+    programmingLanguage  + ' ' +
+    courseCreatorName  + ' ' +
+    reasonText  + ' ' +
+    numberOfMonth 
+);
