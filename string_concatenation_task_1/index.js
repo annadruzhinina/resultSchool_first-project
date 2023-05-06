40a2cd47dc862cd32efcdc76c8bd2a3f078c1f6f
anna <anna@rcbd.org>
2023-05-05 22:55:58 -0400

    Complete task 2

--- string_concatenation_task_1/index.js
+++ string_concatenation_task_1/index.js
@@ -1,17 +1,40 @@
 const myName = "Anna";
-const programmingLanguage = "Java Script";
+const programmingLanguage = "JavaScript";
 const courseCreatorName = "Course Creater Name";
 const reasonText = "Get new knowledge";
 const numberOfMonth = "8 months";
 
+let myInfoText = `${myName} ${programmingLanguage} ${courseCreatorName} ${reasonText} ${numberOfMonth}`;
+
+// Task 1
 console.log(
   `${myName} ${programmingLanguage} ${courseCreatorName} ${reasonText} ${numberOfMonth}`
 );
 
 console.log(
-  myName + ' ' +
-    programmingLanguage  + ' ' +
-    courseCreatorName  + ' ' +
-    reasonText  + ' ' +
-    numberOfMonth 
+  myName +
+    " " +
+    programmingLanguage +
+    " " +
+    courseCreatorName +
+    " " +
+    reasonText +
+    " " +
+    numberOfMonth
+);
+
+// Task 2
+console.log(myInfoText.replaceAll("JavaScript", "javascript"));
+console.log(myInfoText.toUpperCase("Course", "course"));
+// Task 3
+console.log(myInfoText);
+// Task 4
+console.log(myInfoText.length);
+// Task 5
+console.log(
+  "First letter: " +
+    myInfoText[0] +
+    ", " +
+    "Last letter: " +
+    myInfoText[myInfoText.length - 1]
 );
