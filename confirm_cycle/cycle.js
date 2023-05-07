f81decdb6fafddf8747582c707265c750a9b991b
anna <anna@rcbd.org>
2023-05-07 19:22:44 -0400

    Complete while, do whole method

---
+++ confirm_cycle/cycle.js
@@ -0,0 +1,16 @@
+// for
+for (let i = 0; i < 10; i++) {
+  console.log("i = ", i);
+}
+// while
+let j = 0;
+while (j < 10) {
+  console.log(j);
+  j++;
+}
+// do while
+let k = 0;
+do {
+  console.log("k", k);
+  k++;
+} while (k <= 5);
