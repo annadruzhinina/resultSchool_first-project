896d9c9eb79b200c63670505e7531a464bbdb44f
anna <anna@rcbd.org>
2023-05-08 22:57:43 -0400

    Complete task_2

---
+++ Debag/task_2/index.js
@@ -0,0 +1,19 @@
+const temperatureInCelsius = parseInt(
+  prompt("Введите температуру в градусах Цельсия")
+);
+console.log("prompt", typeof temperatureInCelsius);
+
+if (temperatureInCelsius === 0) {
+  alert("0 градусов по Цельсию - это температура замерзания воды");
+} else if (temperatureInCelsius <= 0) {
+  alert(
+    "Для замерзания воды температура должна быть 0 градусов по Цельсию либо ниже"
+  );
+}
+
+const temperatureInFahrenheit = (temperatureInCelsius * 9) / 5 + 32;
+console.log(temperatureInFahrenheit);
+// caught ReferenceError: temperatureIncelsius is not defined
+alert(
+  `${temperatureInCelsius} градусов по Цельсию - это ${temperatureInFahrenheit} по Фаренгейту.`
+);
