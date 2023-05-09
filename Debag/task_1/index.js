f78fc078b9b1940893b49684236e13173a90df7a
anna <anna@rcbd.org>
2023-05-08 22:46:36 -0400

    Complete task1

--- Debag/task_1/index.js
+++ Debag/task_1/index.js
@@ -1,9 +1,12 @@
-let health = prompt('Введите число параметра "здоровье" для персонажа');
-if (health < 0 || health) {
-  console.log("if statment");
+let health = parseInt(
+  prompt('Введите число параметра "здоровье" для персонажа')
+);
+console.log(health, typeof health);
+if (health < 0) {
+  console.log("if statment", health);
 
   alert('Параметр "здоровье" должен быть больше нуля!');
 } else {
   console.log("else");
-  alert(`Параметр "здоровье" равен ${Health}`);
+  alert(`Параметр "здоровье" равен ${health}`);
 }
