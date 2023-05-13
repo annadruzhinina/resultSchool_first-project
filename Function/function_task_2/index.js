08d32e1e3c45cb173f83cdd140f1a68428a811e1
anna <anna@rcbd.org>
2023-05-13 09:28:50 -0400

    Complete task_2

---
+++ Function/function_task_2/index.js
@@ -0,0 +1,21 @@
+function getSumOfNumbers(number, type = "odd") {
+  if (typeof number !== "number") {
+    return NaN;
+  }
+
+  let sum = 0;
+  for (let i = 0; i <= number; i++) {
+    if (type === "odd" && i % 2 !== 0) {
+      sum += i;
+    } else if (type === "even" && i % 2 === 0) {
+      sum += i;
+    } else if (type === "") {
+      sum += i;
+    }
+  }
+
+  return sum;
+}
+console.log(getSumOfNumbers(10, "odd")); // 25
+console.log(getSumOfNumbers(10, "even")); // 30
+console.log(getSumOfNumbers(10, "")); // 55
