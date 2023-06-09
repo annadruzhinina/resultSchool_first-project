f1b30fad3f6890b109d6c8622a1bcd6909ec00ed
anna <anna@rcbd.org>
2023-06-09 12:21:33 -0400

    Update task_2

--- week_3/task_2/index.js
+++ week_3/task_2/index.js
@@ -19,13 +19,18 @@ function makeDomestic(isDomestic) {
   console.log(`${this.type} по имени ${this.name} говорит ${this.makeSound()}`);
   return this;
 }
+makeDomestic.bind(dog, true)();
+// Выводит: "Собака по имени Чарли говорит Гав-Гав"
+// Возвращает: {name: 'Чарли', type: 'Собака', isDomestic: true, makeSound: ƒ}
+const dogResult = makeDomestic.bind(dog, true)();
+console.log(dogResult);
 
-const domesticDog = makeDomestic.bind(dog, true)();
+makeDomestic.call(bird, false);
+// Выводит: "Воробей по имени Петя говорит Чик-чирик"
+// Возвращает: {name: 'Петя', type: 'Воробей', isDomestic: false, makeSound: ƒ}
+const birdResult = makeDomestic.bind(bird, true)();
+console.log(dogResult);
 
-const wildBird = makeDomestic.call(bird, false);
-
-makeDomestic.bind(dog)();
-
-makeDomestic.call(dog);
-
-makeDomestic.apply(dog);
+makeDomestic.apply(bird, [false]);
+// Выводит: "Воробей по имени Петя говорит Чик-чирик"
+// Возвращает: {name: 'Петя', type: 'Воробей', isDomestic: false, makeSound: ƒ}
