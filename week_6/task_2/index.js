2f31c07feec25a1fbab122ec679a7c7fda3a4568
anna <anna@rcbd.org>
2023-06-27 00:21:25 -0400

    Complete task 2

---
+++ week_6/task_2/index.js
@@ -0,0 +1,12 @@
+import IMAGE from "./assets/download.png";
+import "./index.css";
+console.log("Hello world!");
+
+const customText = document.createElement("h1");
+customText.innerText = "I love JavaScript";
+document.body.append(customText);
+
+const imgHTML = document.createElement("img");
+imgHTML.src = IMAGE;
+imgHTML.className = "js-image";
+document.body.append(imgHTML);
